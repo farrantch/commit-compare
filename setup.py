464c373f3ede19ce24a2e104590776de01c819db
Chase Farrant <chase.farrant@artisantechgroup.com>
2022-03-14 15:13:28 -0500

    add labcorp container

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CommitCompare',
+    version='1.0.0',
+    url='https://github.com/farrantch/commit-compare.git',
+    author='Chase',
+    author_email='farrantch23@gmail.com',
+    description='Description of CommitCompare',
+    packages=find_packages(),
+)
